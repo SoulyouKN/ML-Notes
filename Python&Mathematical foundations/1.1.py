19c7509e13efdc4f38769f2078762447fe60266f
Jeffrey Zhan <zkn730665147.com>
2023-10-02 20:48:40 +0800

    ML-note1

---
+++ Python&Mathematical foundations/1.1.py
@@ -0,0 +1,11 @@
+# 几个基础的概念
+# 有监督学习 & 无监督学习(可以简单理解为是否有标签)
+# 数据收集 >> 数据清洗 >> 特征工程 >> 数据建模 >> 新数据预测
+
+#
+import math
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x = [float(i)/100.0 for i in range(1, 100)]
+
